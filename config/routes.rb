abda14995f9e37929a3c54d1d9625b93b775f5c9
Ahlam <ahlam@Ahlams-MacBook-Pro.local>
2022-02-22 00:27:15 +0300

    add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+ 
+  get "home", to: "application#home"
+  root "application#home"
 end
